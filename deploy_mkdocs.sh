faf5c190706810c97e0e0ae24246d78b8f4c696b
zhangteng <zhang.teng520@hotmail.com>
2025-07-26 22:06:20 +0800

    add deploy bash

--- deploy_mkdocs.sh
+++ deploy_mkdocs.sh
@@ -9,11 +9,20 @@ pipenv run mkdocs build
 echo "[INFO] MkDocs build 完成。"
 
 # 2. 清空目标目录（保留 .git）
-target_dir="zhangteng520.github.io"
-site_dir="site"
 
+target_dir="../website/zhangteng520.github.io"
+site_dir="site"
+git_repo="https://github.com/zhangteng520/zhangteng520.github.io.git"
+
+
+# 只保留 .git 文件夹，其余全部删除
 if [ -d "$target_dir" ]; then
-    find "$target_dir" -mindepth 1 ! -name ".git" -exec rm -rf {} +
+    for item in "$target_dir"/* "$target_dir"/.*; do
+        name=$(basename "$item")
+        if [ "$name" != "." ] && [ "$name" != ".." ] && [ "$name" != ".git" ]; then
+            rm -rf "$item"
+        fi
+    done
 else
     mkdir -p "$target_dir"
 fi
@@ -25,14 +34,15 @@ cp -r $site_dir/* $target_dir/
 
 echo "[INFO] 文件已复制到 $target_dir。"
 
+
 # 4. Git 操作
 cd $target_dir
 git rev-parse --is-inside-work-tree
 pwd
-git clone https://github.com/zhangteng520/zhangteng520.github.io.git
+
 git add . -f
 git commit -m "auto: update site $(date '+%Y-%m-%d %H:%M:%S')"
-git push -f https://github.com/zhangteng520/zhangteng520.github.io.git main
+git push -f $git_repo main
 if [ $? -ne 0 ]; then
     echo "[ERROR] Git push 失败，请检查网络连接或权限设置。"
     exit 1
